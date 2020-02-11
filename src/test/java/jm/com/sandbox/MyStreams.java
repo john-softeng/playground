f00432907622671a6dc026c2ba4f2f7dd5ec1154
johnmartech <jjm321@gmail.com>
2020-02-11 11:03:44 -0500

    add comments to MyStreams class

--- src/test/java/jm/com/sandbox/MyStreams.java
+++ src/test/java/jm/com/sandbox/MyStreams.java
@@ -31,25 +31,37 @@ public class MyStreams {
 		
 		System.out.println("\n");
 		
+		// sorted - by natural order
+		// sequential
+		// What's the difference?
 		nameList.stream().sorted().sequential().forEach(System.out::println);
 		System.out.println("\n");
 		
+		// distinct - 
+		// sorted - 
+		// collect - results returned to a new list
 		List<String> newList = nameList.stream().distinct().sorted().collect(Collectors.toList());
+		System.out.println("Same list?  " + (nameList == newList));
+        // Print the new list
 		newList.forEach(System.out::println);
 		System.out.println("\n");
 		
-		
+		// filter - include all strings with the letter "o"
+		// then print
 		nameList.stream().filter(a -> a.contains("o")).forEach(System.out::println);
 		System.out.println("\n");
 		
 		
+        // filter - include all strings with the letter "o"
+		// collect - return a set (note that a set contains unique values.
 		Set<String> set = nameList.stream().filter(a -> a.contains("o")).collect(Collectors.toSet());
 		set.stream().forEach(System.out::println);
 		System.out.println("\n");
 
 	
-		String delList = nameList.stream().collect(Collectors.joining("|"));
-		System.out.println(delList);
+		// joining - creates a string from the list and adds a "|" (delimiter) between each
+		String delimitedList = nameList.stream().collect(Collectors.joining("|"));
+		System.out.println(delimitedList);
 		
 
 	}
